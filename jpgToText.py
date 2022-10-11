8a4c093652d06d1c20f0adc7f5d2d173f521d58f
Arshiya Sharma <71538893+arshiyasharma@users.noreply.github.com>
2022-10-11 16:13:59 +0530

    Add files via upload

---
+++ jpgToText.py
@@ -0,0 +1,8 @@
+import pytesseract as pyt
+
+extracted_text = pyt.image_to_string('ocr.jpg')
+
+file = open('ocr.txt', 'w')  # modes - r=read w=read,write(overwrites) a=read,write,append(adds to prev text)
+file.write(extracted_text)
+file.close()
+print("Text Extracted Successfully")
